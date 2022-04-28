062061d50b1746faa783a3b501fbcd82c221e012
davidecolapietro <davide.colapietro@ennova-research.com>
2022-04-28 14:47:35 +0200

    Weather App (API Project)

---
+++ Weather App/script.js
@@ -0,0 +1,52 @@
+let weather = {
+    apiKey: "39f3167a3bf8ba8a99d5442746c20fb3",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Meteo a " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Umidità: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Vento: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function(event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("Sammichele di Bari");

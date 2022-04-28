ab5458f1801df8a02e0ffe9ae2378ba412000f00
davidecolapietro <davide.colapietro@ennova-research.com>
2022-04-28 15:22:43 +0200

    Weather App (API Project)

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "39f3167a3bf8ba8a99d5442746c20fb3",
+    apiKey: "*************************",
     fetchWeather: function(city) {
         fetch(
                 "https://api.openweathermap.org/data/2.5/weather?q=" +
